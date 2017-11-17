1d589dec99dd3643a0e6fe9d9f04127e1a483e2a
ming3422 <ming3422@qq.com>
2017-11-17 07:06:12 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-      <link rel="stylesheet" href="/css/app.css">
-    <title>@yield('title', 'Sample')  - Laravel 入门教程 </title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-      @include('layouts._header')
+    @include('layouts._header')
 
-
-   <div class="container">
-       @yield('content')
-
-
-   @include('layouts._footer')
-</div>
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
   </body>
 </html>
